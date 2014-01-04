a41de9cb69ed759617cef299836a03ee6c71147b
Klemens Gordon <github@k94n.com>
2014-01-04 13:27:25 +0100

    27" portrait

--- getScreen.js
+++ getScreen.js
@@ -7,9 +7,10 @@ function getScreen(from, to) {
 
     page.viewportSize = { width: 1366, height: 768 };
     page.viewportSize = { width: 1920, height: 1200 };
+    page.viewportSize = { width: 1440, height: 2560 };
     page.open('http://duro/gfda-wp/gfda.html#' + from, function (){
         
-        page.render('screens/gfda-render-1920-' + from + '.png');
+        page.render('screens/gfda-render-' + page.viewportSize.width + 'x' + page.viewportSize.height + '/gfda-render-' + page.viewportSize.width + 'x' + page.viewportSize.height + '-' + from + '.png');
     
         if(from < to) {
             getScreen(from + 1, to);
@@ -19,4 +20,4 @@ function getScreen(from, to) {
     });
 }
 
-getScreen(1,100);
+getScreen(1,2);
